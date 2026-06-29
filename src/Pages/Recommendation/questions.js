08e59fad2e55fbd02d104cd377b33cbab755f0db
mohamed heiba <mohamedheibamedo23@gmail.com>
2026-06-29 15:28:35 +0300

    Fix duplicate states

--- src/Pages/Recommendation/questions.js
+++ src/Pages/Recommendation/questions.js
@@ -2,7 +2,7 @@ const questions = [
   "What are you looking for? (Movie / Game / Series)",
   "What genre do you prefer?",
   "Do you prefer New or Old?",
-  "Do you care about IMDb rating?",
+  "Do you care about GIRA rating?",
 ];
 
 export default questions;
